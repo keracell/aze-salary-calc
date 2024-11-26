782a6ffa1e6ab656a42983fe235bb1057cf73c99
Agil Kerimov (IT) <AKerimov@azercell.com>
2024-11-26 11:50:52 +0400

    remove router

--- src/router/index.ts
+++
@@ -1,8 +0,0 @@
-import { createRouter, createWebHistory } from "vue-router";
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: "/dist/", name: "home", component: () => import("@/views/HomeView.vue") }],
-});
-
-export default router;
